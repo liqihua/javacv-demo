2fd54a4f1926e7c168cb8d48cc3df0de0bf934d6
qihua.li <qihua.li@wehotelglobal.com>
2020-03-17 18:43:55 +0800

    liqihua

---
+++ src/test/java/com/liqihua/demo/javacv/JavacvDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liqihua.demo.javacv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavacvDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
